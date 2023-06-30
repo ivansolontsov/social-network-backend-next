bdd2fd4a3bb19fd8bc5bd30f9c051f6b2e80fd29
Ivan Vasil <ivansolontsov@yandex.ru>
2023-06-30 22:17:07 +0400

    ssl test

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "./pipes/validation.pipe";
 import * as cors from 'cors';
+import fs from 'fs';
+
+
 
 async function start() {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule);
+
+    const httpsOptions = {
+        key: process.env.NODE_ENV == "production" && fs.readFileSync('/etc/letsencrypt/live/vm687554.vps.masterhost.tech/privkey.pem'),
+        cert: process.env.NODE_ENV == "production" && fs.readFileSync('/etc/letsencrypt/live/vm687554.vps.masterhost.tech/fullchain.pem'),
+    };
+
+    const app = await NestFactory.create(AppModule, { httpsOptions });
+
+
+    // Certificate is saved at: /etc/letsencrypt/live/vm687554.vps.masterhost.tech/fullchain.pem
+    // Key is saved at:         /etc/letsencrypt/live/vm687554.vps.masterhost.tech/privkey.pem
+
 
     app.enableCors({
         origin: ['http://localhost:3000', 'https://social-network-alpha-ten.vercel.app'],
